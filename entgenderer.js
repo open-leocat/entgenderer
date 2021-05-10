8e1944d354da52c4f596673d858e94c94b23f67d
open-leocat <68399149+open-leocat@users.noreply.github.com>
2021-05-10 19:45:21 +0200

    Finish "entgenderer.js"
    Usage:
    var remover = new GenderRemover("Lehrer*Innen");   /* Initialize */
    
    console.log(remover.process());                                     /* Process, print result */

--- entgenderer.js
+++ entgenderer.js
@@ -1,3 +1,45 @@
-export function cleanFromGender(content) {
-  return "hello world!";
+class GenderRemover {
+    constructor(text) {
+        this.text = text;
+        this.pos = -1;
+        this.currentChar = '\0';
+        this.advance();
+    }
+    
+    advance() {
+        this.pos++;
+        if(this.pos < this.text.length) {
+            this.currentChar = this.text.charAt(this.pos);
+        } else {
+            this.currentChar = '\0';
+        }
+    }
+    
+    process() {
+        var temp = "";
+        
+        while(this.currentChar != '\0') {
+            if(":/*_".includes(this.currentChar)) {
+                while(!" \t.,;".includes(this.currentChar)) {
+                    this.advance();
+                }
+            } else if(this.currentChar == 'I') {
+                if(this.text.charAt(this.pos+1) == 'n') {
+                    while(!" \t.,;".includes(this.currentChar)) {
+                        this.advance();
+                    }
+                } else {
+                    temp += this.currentChar;
+                    
+                    this.advance();
+                }
+            } else {
+                temp += this.currentChar;
+                
+                this.advance();
+            }
+        }
+        
+        return temp;
+    }
 }
