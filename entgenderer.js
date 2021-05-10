1ed9994b822a9eee1018f31a5af049539471d864
open-leocat <68399149+open-leocat@users.noreply.github.com>
2021-05-10 21:26:52 +0200

    Update entgenderer.js

--- entgenderer.js
+++ entgenderer.js
@@ -1,45 +1,51 @@
 class GenderRemover {
-    constructor(text) {
-        this.text = text;
-        this.pos = -1;
-        this.currentChar = '\0';
-        this.advance();
-    }
-    
-    advance() {
-        this.pos++;
-        if(this.pos < this.text.length) {
-            this.currentChar = this.text.charAt(this.pos);
-        } else {
-            this.currentChar = '\0';
-        }
-    }
-    
-    process() {
-        var temp = "";
-        
-        while(this.currentChar != '\0') {
-            if(":/*_".includes(this.currentChar)) {
-                while(!" \t.,;\0\n".includes(this.currentChar)) {
-                    this.advance();
-                }
-            } else if(this.currentChar == 'I') {
-                if(this.text.charAt(this.pos+1) == 'n') {
-                    while(!" \t.,;\0\n.includes(this.currentChar)) {
-                        this.advance();
-                    }
-                } else {
-                    temp += this.currentChar;
-                    
-                    this.advance();
-                }
-            } else {
-                temp += this.currentChar;
-                
-                this.advance();
-            }
-        }
-        
-        return temp;
-    }
+          constructor(text) {
+              this.text = text;
+              this.pos = -1;
+              this.currentChar = '\0';
+              this.advance();
+          }
+
+          advance() {
+              this.pos++;
+              if(this.pos < this.text.length) {
+                  this.currentChar = this.text.charAt(this.pos);
+              } else {
+                  this.currentChar = '\0';
+              }
+          }
+
+          process() {
+              var temp = "";
+
+              while(this.currentChar != '\0') {
+                  if(":/*_".includes(this.currentChar)) {
+                  	  if(this.currentChar == 'n') {
+                      	  temp += this.currentChar;
+                          
+                          advance();
+                      } else {
+                      	  while(!" \t.,;\0\n".includes(this.currentChar)) {
+                          	  this.advance();
+                      	  }
+                      }
+                  } else if(this.currentChar == 'I') {
+                      if(this.text.charAt(this.pos+1) == 'n') {
+                          while(!" \t.,;\0\n".includes(this.currentChar)) {
+                              this.advance();
+                          }
+                      } else {
+                          temp += this.currentChar;
+
+                          this.advance();
+                      }
+                  } else {
+                      temp += this.currentChar;
+
+                      this.advance();
+                  }
+              }
+
+              return temp;
+          }
 }
